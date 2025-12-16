3cfc0a2df9d7a324f71e3dac79886bf1d8bd7529
tbdavid2019 <tbdavid2019@gmail.com>
2025-12-16 15:57:05 +0800

    chore: Update project links to new GitHub Pages domain and repository.

--- src/data/templates.js
+++ src/data/templates.js
@@ -7,7 +7,7 @@ module.exports = [
       title: '數位名片新功能上線',
       place: '線上',
       time: '2023/12/26 15:00',
-      url: 'https://github.com/taichunmin/liff-businesscard',
+      url: 'https://github.com/tbdavid2019/line-liff-namecard',
       imageUrl: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
       description: '我們剛剛更新了數位名片的功能！\n新增了 Flex Message 樣板支援，讓您可以更方便地分享資訊。\n\n快來試試看吧！',
       activity: '系統更新'
@@ -21,7 +21,7 @@ module.exports = [
       title: '開發團隊名單',
       place: '台北市',
       imageUrl: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_2_restaurant.png',
-      url: 'https://github.com/taichunmin',
+      url: 'https://github.com/tbdavid2019',
       activity: '團隊成員',
       map: 'https://maps.google.com',
       people: [
@@ -40,7 +40,7 @@ module.exports = [
       description: '全端工程師\n熱愛開源專案與新技術',
       avatar: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
       back: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
-      followUrl: 'https://github.com/taichunmin'
+      followUrl: 'https://github.com/tbdavid2019'
     }
   },
   // Template 4 - News
@@ -51,7 +51,7 @@ module.exports = [
       image: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
       date: '2023.12.16',
       description: '【重要通知】API 服務維護公告',
-      link: 'https://github.com/taichunmin/liff-businesscard',
+      link: 'https://github.com/tbdavid2019/line-liff-namecard',
       tag: 'News',
       targetPicker: 'Line'
     }
